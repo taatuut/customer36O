6642bd6d9fabc230a4c0d69ba96d11af3a76281f
Emil Zegers <e.zegers@upcmail.nl>
2024-08-15 16:02:19 +0200

    Add files via upload

---
+++ src/main/java/Application.java
@@ -0,0 +1,51 @@
+
+
+import java.util.function.Consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	@Autowired
+	private StreamBridge streamBridge;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+
+	@Bean
+	public Consumer<CustomerJson> acmeretailOnlineservicesCustomerCreatedV2RegionIdCustomerIdConsumer() {
+		return data -> {
+			// Add business logic here.	
+			logger.info(data.toString());
+		};
+	}
+
+	@Bean
+	public Consumer<OrderJson> acmeretailOnlineservicesOrderCreatedV2RegionIdOrderIdConsumer() {
+		return data -> {
+			// Add business logic here.	
+			logger.info(data.toString());
+		};
+	}
+
+	@Bean
+	public Consumer<CustomerJson> acmeretailOnlineservicesCustomerUpdatedV2RegionIdCustomerIdConsumer() {
+		return data -> {
+			// Add business logic here.	
+			logger.info(data.toString());
+		};
+	}
+
+}
